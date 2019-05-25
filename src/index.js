ae7f4cc5d6711f702ad7d64640885135cecf7cb3
sergye <sergye@gmail.com>
2019-05-25 12:36:56 +0300

    Stage 3. Add debug

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 import fs from 'mz/fs';
 import path from 'path';
+import debug from 'debug';
 import axios from './lib/axios';
 import getName from './lib/name_formater';
 import parseLinks from './lib/link_parcer';
 import showError from './lib/error_informer';
 import loadResources from './lib/resource_loader';
 
-export default (pageURL, outputPath = '.') => {
-  const pageName = path.resolve(outputPath, getName('getPageName', pageURL));
-  const recourcesDir = path.resolve(outputPath, getName('getFolderName', pageURL));
+const log = debug('page-loader:main');
+
+export default (url, outputPath = '.') => {
+  const pageName = path.resolve(outputPath, getName('getPageName', url));
+  const recourcesDir = path.resolve(outputPath, getName('getFolderName', url));
+  log(`Start page-loader \n  url = ${url} \n  outputPath = ${outputPath}`);
 
   return axios
-    .get(pageURL)
+    .get(url)
     .then(result => result.data)
     .then((data) => {
       const links = parseLinks(data);
-      const fullLinks = links.map(link => getName('getFullLink', pageURL, link));
+      const fullLinks = links.map(link => getName('getFullLink', url, link));
       const localPageData = links.reduce((acc, link) => acc.replace(link, getName('getLocalLink', recourcesDir, link)), data);
       return Promise.all([
         loadResources(fullLinks, recourcesDir),
         fs.writeFile(pageName, localPageData)]);
     })
-    .then(() => `OK: Data has been downloaded from ${pageURL} to ${pageName}\n`)
-    .catch(error => Promise.reject(new Error(showError(pageURL, error))));
+    .then(() => `OK: Data has been downloaded from ${url} to ${pageName}\n`)
+    .catch(error => Promise.reject(new Error(showError(url, error))));
 };
--- src/lib/resource_loader.js
+++ src/lib/resource_loader.js
@@ -1,8 +1,11 @@
 import fs from 'mz/fs';
 import path from 'path';
+import debug from 'debug';
 import axios from './axios';
 import getName from './name_formater';
 import showError from './error_informer';
+
+const log = debug('page-loader:resources');
 
 const getResource = (url, dir) => {
   axios.get(url, { responseType: 'arraybuffer' })
@@ -13,5 +16,7 @@ const getResource = (url, dir) => {
 export default (urls, dir) => {
   fs.exists(dir)
     .then(exists => (exists ? Promise.resolve() : fs.mkdir(dir)))
-    .then(() => Promise.all(urls.map(url => getResource(url, dir))));
+    .then(() => log(`Download started \nDirectory: ${dir}`))
+    .then(() => Promise.all(urls.map(url => getResource(url, dir))))
+    .then(() => log('Download finished'));
 };
