0771e5980ffc885bb386e4727f910846db3db666
sergye <sergye@gmail.com>
2019-05-26 09:01:42 +0300

    Stage 5. Add Listr

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import fs from 'mz/fs';
 import path from 'path';
 import debug from 'debug';
+import Listr from 'listr';
 import axios from './lib/axios';
 import getName from './lib/name_formater';
 import parseLinks from './lib/link_parcer';
@@ -14,17 +15,32 @@ export default (url, outputPath = '.') => {
   const recourcesDir = path.resolve(outputPath, getName('getFolderName', url));
   log(`Start page-loader \n  url = ${url} \n  outputPath = ${outputPath}`);
 
-  return axios
-    .get(url)
-    .then(result => result.data)
-    .then((data) => {
-      const links = parseLinks(data);
-      const fullLinks = links.map(link => getName('getFullLink', url, link));
-      const localPageData = links.reduce((acc, link) => acc.replace(link, getName('getLocalLink', recourcesDir, link)), data);
-      return Promise.all([
-        loadResources(fullLinks, recourcesDir),
-        fs.writeFile(pageName, localPageData)]);
-    })
+  const tasks = new Listr([
+    {
+      title: 'Page loading',
+      task: ctx => axios.get(url)
+        .then((result) => {
+          ctx.data = result.data;
+        }),
+    },
+    {
+      title: 'Resources loading',
+      task: (ctx) => {
+        const { data } = ctx;
+        const links = parseLinks(data);
+        const fullLinks = links.map(link => getName('getFullLink', url, link));
+        const localPageData = links.reduce((acc, link) => acc.replace(link, getName('getLocalLink', recourcesDir, link)), data);
+        return fs.writeFile(pageName, localPageData)
+          .then(() => loadResources(fullLinks, recourcesDir))
+          .then((results) => {
+            ctx.msgs = results;
+          });
+      },
+    },
+  ]);
+
+  return tasks.run()
+    .then(ctx => ctx.result)
     .then(() => `OK: Data has been downloaded from ${url} to ${pageName}\n`)
     .catch(error => showError(url, error));
 };
