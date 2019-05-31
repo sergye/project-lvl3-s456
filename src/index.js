089d8557c98f7ca6ad8fef035c00df508db2e34c
sergye <sergye@gmail.com>
2019-05-31 13:32:00 +0300

    Refactor after mentor's code review

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
-import fs from 'mz/fs';
+import fs from 'fs';
+import { promisify } from 'util';
 import path from 'path';
 import debug from 'debug';
 import Listr from 'listr';
 import axios from './lib/axios';
-import getName from './lib/name_formater';
-import parseLinks from './lib/link_parcer';
-import showError from './lib/error_informer';
-import loadResources from './lib/resource_loader';
+import getName from './lib/nameFormater';
+import parseLinks from './lib/linkParser';
+import showError from './lib/errorInformer';
+
+const writeFile = promisify(fs.writeFile);
+const fileExists = promisify(fs.exists);
+const mkdir = promisify(fs.mkdir);
 
 const log = debug('page-loader:main');
+
+const loadResources = (urls, dir) => {
+  const getResource = (url, folder) => {
+    axios.get(url, { responseType: 'arraybuffer' })
+      .then(result => writeFile(path.resolve(folder, getName('getFileName', url)), result.data))
+      .catch(error => showError(url, error));
+  };
+  fileExists(dir)
+    .then(exists => (exists ? Promise.resolve() : mkdir(dir)))
+    .then(() => log(`Download started to directory: ${dir}`))
+    .then(() => Promise.all(urls.map(url => getResource(url, dir))));
+};
 
 export default (url, outputPath = '.') => {
   const pageName = path.resolve(outputPath, getName('getPageName', url));
   const recourcesDir = path.resolve(outputPath, getName('getFolderName', url));
-  log(`Start page-loader \n  url = ${url} \n  outputPath = ${outputPath}`);
+  log(`Start page-loader, url = ${url}, outputPath = ${outputPath}`);
 
   const tasks = new Listr([
     {
@@ -30,7 +46,7 @@ export default (url, outputPath = '.') => {
         const links = parseLinks(data);
         const fullLinks = links.map(link => getName('getFullLink', url, link));
         const localPageData = links.reduce((acc, link) => acc.replace(link, getName('getLocalLink', recourcesDir, link)), data);
-        return fs.writeFile(pageName, localPageData)
+        return writeFile(pageName, localPageData)
           .then(() => loadResources(fullLinks, recourcesDir))
           .then((results) => {
             ctx.msgs = results;
